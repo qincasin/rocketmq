b0ae4a2cab5be38b633000d1ba391c382fde6c9a
qincasin <qincasin@163.com>
2022-11-20 14:10:05 +0800

    doc: GetMessageStatus状态关系 https://raw.githubusercontent.com/qincasin/oss/main/uPic/20221120/消费者-服务器-Store 状态码对照表.jpg

--- store/src/main/java/org/apache/rocketmq/store/GetMessageStatus.java
+++ store/src/main/java/org/apache/rocketmq/store/GetMessageStatus.java
@@ -16,23 +16,36 @@
  */
 package org.apache.rocketmq.store;
 
+/**
+ * ![消费者-服务器-Store状态码对照表](https://raw.githubusercontent.com/qincasin/oss/main/uPic/20221120/消费者-服务器-Store 状态码对照表.jpg)
+ */
 public enum GetMessageStatus {
 
+    //查询出来的MSG
     FOUND,
 
+    // 未查询到消息：服务端消息过滤tagCode
     NO_MATCHED_MESSAGE,
 
+    //查询时 正好赶上CommitLog 清理过期文件，导致查询失败
     MESSAGE_WAS_REMOVING,
 
+    //未查询
     OFFSET_FOUND_NULL,
 
+
+    //pullRequest.offset 越界 maxOffset
     OFFSET_OVERFLOW_BADLY,
 
+    //pullRequest.offset == CQ.maxOffset
     OFFSET_OVERFLOW_ONE,
 
+    //pullRequest.offset 越界 minbOffset
     OFFSET_TOO_SMALL,
 
+    //不存在这种情况
     NO_MATCHED_LOGIC_QUEUE,
 
+    //空队列，创建队列也是因为查询到导致的
     NO_MESSAGE_IN_QUEUE,
 }
